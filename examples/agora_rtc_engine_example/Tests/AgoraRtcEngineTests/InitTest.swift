1b6aae11d72e3f67cc577e67f1df2cc1a6deb0af
Chuck Grindel <chuck.grindel@gmail.com>
2023-02-25 12:50:48 -0700

    Initial generated files.

---
+++ examples/agora_rtc_engine_example/Tests/AgoraRtcEngineTests/InitTest.swift
@@ -0,0 +1,13 @@
+@testable import AgoraRtcKit
+import XCTest
+
+class InitTest: XCTestCase {
+    let appID = "12345"
+    func test_init() throws {
+        let config = AgoraRtcEngineConfig()
+        // Pass in your App ID here.
+        config.appId = appID
+        // Use AgoraRtcEngineDelegate for the following delegate parameter.
+        agoraEngine = AgoraRtcEngineKit.sharedEngine(with: config, delegate: nil)
+    }
+}
--- examples/agora_rtc_engine_example/swift_deps.bzl
+++ examples/agora_rtc_engine_example/swift_deps.bzl
@@ -1,2 +1,10 @@
+load("@cgrindel_swift_bazel//swiftpkg:defs.bzl", "swift_package")
+
 def swift_dependencies():
-    pass
+    # version: 4.1.1
+    swift_package(
+        name = "swiftpkg_agorartcengine_ios",
+        commit = "13f007dff53e2c670ad645fae43e3fcea8f4df6c",
+        dependencies_index = "@//:swift_deps_index.json",
+        remote = "https://github.com/AgoraIO/AgoraRtcEngine_iOS",
+    )
